cb8a18c205185c613c1495fcb51d601f7f4e1347
Seth Brady <seth.brady@framebridge.com>
2021-02-03 22:26:12 -0500

    Added mock resource

---
+++ src/Components/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+    </ul>
+</template>
+
+<script>
+    export default {
+      data(){
+          return {
+              storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Offical Guide',
+                    description: 'The official Vue JS Documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'googlr',
+                    title: 'Googlr',
+                    description: 'Google',
+                    link: 'www.google.com'
+                }
+              ]
+          };
+      }
+    }
+</script>
