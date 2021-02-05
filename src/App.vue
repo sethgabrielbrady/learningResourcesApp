b468b2c76cd4adda787bd884887a8fd684337aa1
Seth Brady <seth.brady@framebridge.com>
2021-02-04 23:12:19 -0500

    Adds new resources component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,49 @@
 <template>
-    <ul>
-        <learning-resource v-for="res in storedResources" :key="res.id" :title="res.title" :description="res.link" :link="res.link"></learning-resource>
-    </ul>
+  <div>
+    <stored-resources :resources="storedResources"> </stored-resources>
+  </div>
 </template>
 
 <script>
-    import LearningResource from './Components/learning-resources/LearningResource.vue';
+  import StoredResources from './Components/learning-resources/StoredResources.vue';
 
-    export default {
-     components: {
-        LearningResource
+  export default {
+    components: {
+        StoredResources
     },
     data(){
-          return {
-              storedResources: [
-                {
-                    id: 'official-guide',
-                    title: 'Offical Guide',
-                    description: 'The official Vue JS Documentation',
-                    link: 'https://vuejs.org'
-                },
-                {
-                    id: 'google',
-                    title: 'Google',
-                    description: 'Google',
-                    link: 'www.google.com'
-                }
-              ]
-          };
-      }
+      return {
+        storedResources: [
+          {
+            id: 'official-guide',
+            title: 'Offical Guide',
+            description: 'The official Vue JS Documentation',
+            link: 'https://vuejs.org'
+          },
+          {
+            id: 'google',
+            title: 'Google',
+            description: 'Google',
+            link: 'www.google.com'
+          }
+        ]
+      };
     }
+  }
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
+</style>
--- src/Components/learning-resources/LearningResource.vue
+++ src/Components/learning-resources/LearningResource.vue
@@ -1,24 +1,49 @@
 <template>
-    <li>
-        <div>
-            <header>
-            <h3>{{ title}}</h3>
-            <button>Delete</button>
-            </header>
-        </div>
-        <p>{{ description}}</p>
-        <nav>
-            <a :href="link">View Resource</a>
-        </nav>
-    </li>
+  <div>
+    <stored-resources></stored-resources>
+  </div>
 </template>
 
 <script>
-export default {
-    props: ['title', 'description', 'link']
-}
+  import StoredResources from './Components/learning-resources/StoredResources.vue';
+
+  export default {
+    components: {
+        StoredResources
+    },
+    data(){
+      return {
+        resources: [
+          {
+            id: 'official-guide',
+            title: 'Offical Guide',
+            description: 'The official Vue JS Documentation',
+            link: 'https://vuejs.org'
+          },
+          {
+            id: 'google',
+            title: 'Google',
+            description: 'Google',
+            link: 'www.google.com'
+          }
+        ]
+      };
+    }
+  }
 </script>
 
-<style scoped>
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
 </style>
